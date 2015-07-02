105a799a15626ccaf08e3816d53fcd14dfbfde64
Robin-Manuel Thiel <robin-manuel.thiel@symanto.net>
2015-07-02 15:29:47 +0200

    Added LoadingIndicatorService.js

---
+++ LoadingIndicatorService.js
@@ -0,0 +1,23 @@
+﻿var root = angular.module("Symanto.Loading");
+root.service("LoadingIndicatorService", function () {
+    var loadingIndicatorService = {
+        "indicatorVisibleStatus": {
+            "visible": false
+        },
+        "hideLoadingIndicator": function () {
+            angular.extend(this.indicatorVisibleStatus, {
+                "visible": false
+            });
+        },
+        "showLoadingIndicator": function () {
+            angular.extend(this.indicatorVisibleStatus, {
+                "visible": true
+            });
+        },
+        "getCurrentState": function () {
+            return this.indicatorVisibleStatus;
+        }
+    };
+
+    return loadingIndicatorService;
+});
