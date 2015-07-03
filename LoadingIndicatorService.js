68f6507136f73733270a2f785a0f7544a6a4121e
Robin-Manuel Thiel <robin-manuel.thiel@symanto.net>
2015-07-03 15:56:10 +0200

    Added own module definition and counter

--- LoadingIndicatorService.js
+++ LoadingIndicatorService.js
@@ -1,23 +1,42 @@
-﻿var root = angular.module("Symanto.Loading");
-root.service("LoadingIndicatorService", function () {
-    var loadingIndicatorService = {
-        "indicatorVisibleStatus": {
-            "visible": false
-        },
-        "hideLoadingIndicator": function () {
-            angular.extend(this.indicatorVisibleStatus, {
-                "visible": false
-            });
-        },
-        "showLoadingIndicator": function () {
-            angular.extend(this.indicatorVisibleStatus, {
-                "visible": true
-            });
-        },
-        "getCurrentState": function () {
-            return this.indicatorVisibleStatus;
+﻿angular.module("Symanto.LoadingIndicator", ["ui.router"]);
+
+var loading = angular.module("Symanto.LoadingIndicator");
+
+loading.service("LoadingIndicatorService", function () {
+
+    var running = 0;
+    var indicatorVisibleStatus = {
+        "visible": false
+    };
+
+    // Count the number of running oprations
+    // Start the loading indicator only at the first one
+    var startOperation = function() {
+        running ++;
+
+        if (running > 0) {
+            angular.extend(this.indicatorVisibleStatus, { "visible": true });
         }
     };
 
-    return loadingIndicatorService;
+    // When an operation is finished, it should be removed from the running sources
+    // Hide the loading indicator, if it was the last one.
+    var finishOperation = function() {
+        running--;
+
+        if (running == 0) {
+            angular.extend(this.indicatorVisibleStatus, { "visible": false });
+        }
+    };
+
+    var getCurrentState = function() {
+        return this.indicatorVisibleStatus;
+    };
+
+    return {
+        "indicatorVisibleStatus": indicatorVisibleStatus,
+        "startOperation": startOperation,
+        "finishOperation": finishOperation,
+        "getCurrentState": getCurrentState
+    };
 });
