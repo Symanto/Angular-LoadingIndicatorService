fdf02cf1dce95e586d3b722a10fecdf159766fc2
Robin-Manuel Thiel <robin-manuel.thiel@symanto.net>
2015-07-06 11:46:56 +0200

    Removed unused element of return

--- LoadingIndicatorService.js
+++ LoadingIndicatorService.js
@@ -9,32 +9,41 @@ loading.service("LoadingIndicatorService", function () {
         "visible": false
     };
 
-    // Count the number of running oprations
-    // Start the loading indicator only at the first one
+    /**
+     * Starts an operation and shows the loading indicator if it was hidden
+     */
     var startOperation = function() {
+        // Increase the  number of running operations
         running ++;
 
+        // Start the loading indicator only at the first one
         if (running > 0) {
             angular.extend(this.indicatorVisibleStatus, { "visible": true });
         }
     };
 
-    // When an operation is finished, it should be removed from the running sources
-    // Hide the loading indicator, if it was the last one.
+    /**
+     * Finishes an operation and decides afterwards if the loading indicator is to hide
+     */
     var finishOperation = function() {
+        // When an operation is finished, it should be removed from the running sources
         running--;
 
+        // Hide the loading indicator, if it was the last one.
         if (running == 0) {
             angular.extend(this.indicatorVisibleStatus, { "visible": false });
         }
     };
 
+    /**
+     * Gets the current loading indicator status
+     * @returns {indicatorVisibleStatus}
+     */
     var getCurrentState = function() {
         return this.indicatorVisibleStatus;
     };
 
     return {
-        "indicatorVisibleStatus": indicatorVisibleStatus,
         "startOperation": startOperation,
         "finishOperation": finishOperation,
         "getCurrentState": getCurrentState
